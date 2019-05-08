144420e75de40b84d9f54390deb076e52ceb867a
0pyro0 <0pyro0@gmail.com>
2019-05-08 05:19:24 +0000

    Automatically backed up by Learn

---
+++ Alice/code.js
@@ -0,0 +1,10 @@
+let image = document.querySelector("#kitten")
+image.onclick = toggleClass;
+
+function toggleClass(){
+  if(image.className == 'image'){
+      image.className = ''
+  } else {
+      image.className = 'image'
+  }
+}
